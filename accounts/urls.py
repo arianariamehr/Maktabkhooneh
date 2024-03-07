cef5a3d6d63ba37ad6af87c5878f4fd65d4135f1
arianariamehr <arian.ariamehr1989@gmail.com>
2024-03-07 08:44:57 +0330

    added account template files

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from accounts.views import *
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', index_view, name='index')
+    path('login', login_view, name='login'),
+    path('logout', logout_view, name='logout'),
+    path('signup', signup_view, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 
-def index_view(request):
-    return render(request, 'accounts/index.html')
+
+def login_view(request):
+    return render(request, 'accounts/login.html')
+
+def logout_view(request):
+    return render(request, 'accounts/logout.html')
+
+def signup_view(request):
+    return render(request, 'accounts/signup.html')
