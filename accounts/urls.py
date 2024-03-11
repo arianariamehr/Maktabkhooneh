ee41d7cbcd4b25be5f1ccd61ddf81f78244375fd
arianariamehr <arian.ariamehr1989@gmail.com>
2024-03-12 02:47:31 +0330

    logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,8 @@ from accounts.views import *
 app_name = 'accounts'
 
 urlpatterns = [
+    path('', login_view, name='login'),
     path('login', login_view, name='login'),
-    #   path('logout', logout_view, name='logout'),
+    path('logout', logout_view, name='logout'),
     path('signup', signup_view, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def login_view(request):
-    return render(request, 'accounts/login.html')
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+
+    form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
 
 
-# def logout_view(request):
-#    return render(request, 'accounts/logout.html')
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+    redirect('/')
 
 
 def signup_view(request):
